c15af2f04b2626c19d22c497942bc8d9cc479020
SchneeWelt <78919789+SchneeWelt@users.noreply.github.com>
2021-03-27 15:02:10 +0100

    Delete tools directory

--- tools/circle/ccircle/OnCollision.java
+++
@@ -1,6 +0,0 @@
-package tools.circle.ccircle;
-
-public interface OnCollision
-{
-	public void onCollision();
-}
--- tools/printer/Printer.java
+++
@@ -1,9 +0,0 @@
-package tools.printer;
-
-public class Printer
-{
-	public void print(String message)
-	{
-		System.out.println(message);
-	}
-}
